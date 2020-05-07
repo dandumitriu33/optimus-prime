836044664cb7c40f800597c25ccd096de41bf97d
dandumitriu33 <dandumitriu33@gmail.com>
2020-05-07 20:24:04 +0300

    Add improvement to isPrime() method

--- src/com/codecool/com/codecool/com/codecool/Main.java
+++ src/com/codecool/com/codecool/com/codecool/Main.java
@@ -69,7 +69,7 @@ public class Main {
 
     static boolean isPrime(int number) {
         if (number == 2) return true;
-        for (int i=2; i<number; i++) {
+        for (int i=2; i<Math.sqrt(number)+1; i++) {
             if (number%i == 0) return false;
         }
         return true;
